43d3f325d9ff273d27f620bb6826a61651b2386d
Edward <edwardz10@github.com>
2017-11-08 00:11:38 +0300

    Added scopes project

---
+++ angular_ninja/scopes/app/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'myApp.controllers'
+]);
+
+angular.module('myApp').run(function($rootScope) {
+    $rootScope.title = 'Famous Books';
+    $rootScope.name = 'Root scope';
+});
